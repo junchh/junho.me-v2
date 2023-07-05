fd75e9a305b6fbe58c35bd6379fc88ab2a9c948a
junchh <cjunho9868@gmail.com>
2023-07-05 20:45:47 +0700

    add homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,41 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import NavbarColorModeToggle from "@theme/Navbar/ColorModeToggle";
-
 import styles from "./index.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.image}></div>
+        <div className={styles.image}>
+          <img src={require("@site/static/img/profile.jpg").default} />
+        </div>
         <div className={styles.contentText}>
+          <h1>Hello.</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-            dolore vel, eius sint nihil esse maiores fugiat dolorum perferendis.
-            Asperiores eius ipsam perspiciatis impedit tempore eum officiis
-            itaque natus modi?
+            Welcome to this little corner of mine. I craft various interesting
+            gizmo on the web ranging from interactive games to wacky user
+            interfaces, utilizing the power of TypeScript and React (or other
+            frameworks).
           </p>
         </div>
-        <div className={styles.contact}></div>
+        <div className={styles.contact}>
+          <div className={styles.links}>
+            <Link to={"mailto:cjunho9868@gmail.com"}>
+              <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
+            </Link>
+            <Link to={"https://www.linkedin.com/in/junhoch/"}>
+              <FontAwesomeIcon className={styles.icon} icon={faLinkedin} />
+            </Link>
+            <Link to={"https://github.com/junchh"}>
+              <FontAwesomeIcon className={styles.icon} icon={faGithub} />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
